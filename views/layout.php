00af834fdfb83ebcca4366e636b37a42b7b58bda
Livni <llmc0383@gmail.com>
2023-08-05 16:29:36 -0600

    actualizado

--- views/layout.php
+++ views/layout.php
@@ -34,7 +34,7 @@
                         <ul class="dropdown-menu  dropdown-menu-dark "id="dropwdownRevision" style="margin: 0;">
                             <!-- <h6 class="dropdown-header">Información</h6> -->
                             <li>
-                                <a class="dropdown-item nav-link text-white " href="/final_IS2_martinez/grados"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Subitem</a>
+                                <a class="dropdown-item nav-link text-white " href="/final_IS2_martinez/grados"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Grados y Armas</a>
                             </li>
                         
                     
