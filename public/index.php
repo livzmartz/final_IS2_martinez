bfd35dc3db3914e637bdfc58cb39d827a13d0ff4
Livni <llmc0383@gmail.com>
2023-08-05 16:50:47 -0600

    se actualizo el index para la vista del cotrolador de grados y armas

--- public/index.php
+++ public/index.php
@@ -4,10 +4,16 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\GradosController;
+
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
-
+$router->get('/grados', [GradosController::class,'index'] );
+$router->post('/API/grados/guardar', [GradosController::class,'guardarAPI'] );
+$router->post('/API/grados/modificar', [GradosController::class,'modificarAPI'] );
+$router->post('/API/grados/eliminar', [GradosController::class,'eliminarAPI'] );
+$router->get('/API/grados/buscar', [GradosController::class,'buscarAPI'] );
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
