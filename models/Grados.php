ee01a3b07ceb95d203181b9c2791ca600d2c46c8
Livni <llmc0383@gmail.com>
2023-08-05 11:22:04 -0600

    se modifico el modelo de grados

--- models/Grados.php
+++ models/Grados.php
@@ -2,7 +2,20 @@
 
 namespace Model;
 
-class Producto extends ActiveRecord{
-    public static $tabla = 'productos';
-    public static $columnasDB = ['producto_nombre','producto_precio','producto_situacion'];
-    public static $idTabla = 'producto_id';
+class Grados extends ActiveRecord{
+    public static $tabla = 'grados';
+    public static $columnasDB = ['gra_nombre','gra_sit'];
+    public static $idTabla = 'gra_id';
+
+    public $gra_id;
+    public $gra_nombre;
+    public $gra_sit;
+
+    public function __construct($args = [] )
+    {
+        $this->gra_id = $args['gra_id'] ?? null;
+        $this->gra_nombre = $args['gra_nombre'] ?? '';
+        $this->gra_sit = $args['gra_sit'] ?? 1;
+    }
+
+}
