c7c3c0507186ca25ab567b9733fb5e28383ec1b3
Livni <llmc0383@gmail.com>
2023-08-05 17:03:14 -0600

    se agrego el modificar y eliminar

--- src/js/grados/index.js
+++ src/js/grados/index.js
@@ -68,4 +68,197 @@ const guardar = async (evento) => {
 }
 
 
+const buscar = async () => {
+
+    let gra_nombre = formulario.gra_nombre.value;
+    
+    const url = `/final_IS2_martinez/API/grados/buscar?gra_nombre=${gra_nombre}`;
+    const config = {
+        method : 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config)
+        const data = await respuesta.json();
+        
+        tablaGrados.tBodies[0].innerHTML = ''
+        const fragment = document.createDocumentFragment();
+        console.log(data);
+        // return;
+        if(data.length > 0){
+            let contador = 1;
+            data.forEach( grados => {
+                // CREAMOS ELEMENTOS
+                const tr = document.createElement('tr');
+                const td1 = document.createElement('td')
+                const td2 = document.createElement('td')
+                const td3 = document.createElement('td')
+                const td4 = document.createElement('td')
+                
+                const buttonModificar = document.createElement('button')
+                const buttonEliminar = document.createElement('button')
+
+                // CARACTERISTICAS A LOS ELEMENTOS
+                buttonModificar.classList.add('btn', 'btn-warning')
+                buttonEliminar.classList.add('btn', 'btn-danger')
+                buttonModificar.textContent = 'Modificar'
+                buttonEliminar.textContent = 'Eliminar'
+
+                buttonModificar.addEventListener('click', () => colocarDatos(grados))
+                buttonEliminar.addEventListener('click', () => eliminar(grados.gra_id))
+
+                td1.innerText = contador;
+                td2.innerText = producto.gra_nombre
+               
+                
+                // ESTRUCTURANDO DOM
+                td3.appendChild(buttonModificar)
+                td4.appendChild(buttonEliminar)
+                tr.appendChild(td1)
+                tr.appendChild(td2)
+                tr.appendChild(td3)
+                tr.appendChild(td4)
+                
+                fragment.appendChild(tr);
+
+                contador++;
+            })
+        }else{
+            const tr = document.createElement('tr');
+            const td = document.createElement('td')
+            td.innerText = 'No existen registros'
+            td.colSpan = 4
+            tr.appendChild(td)
+            fragment.appendChild(tr);
+        }
+
+        tablaGrados.tBodies[0].appendChild(fragment)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const colocarDatos = (datos) => {
+    formulario.gra_nombre.value = datos.gra_nombre
+    formulario.gra_id.value = datos.gra_id
+
+    btnGuardar.disabled = true
+    btnGuardar.parentElement.style.display = 'none'
+    btnBuscar.disabled = true
+    btnBuscar.parentElement.style.display = 'none'
+    btnModificar.disabled = false
+    btnModificar.parentElement.style.display = ''
+    btnCancelar.disabled = false
+    btnCancelar.parentElement.style.display = ''
+    divTabla.style.display = 'none'
+
+    // modalEjemploBS.show();
+}
+
+const cancelarAccion = () => {
+    btnGuardar.disabled = false
+    btnGuardar.parentElement.style.display = ''
+    btnBuscar.disabled = false
+    btnBuscar.parentElement.style.display = ''
+    btnModificar.disabled = true
+    btnModificar.parentElement.style.display = 'none'
+    btnCancelar.disabled = true
+    btnCancelar.parentElement.style.display = 'none'
+    divTabla.style.display = ''
+}
+
+const modificar = async () => {
+    if(!validarFormulario(formulario)){
+        alert('Debe llenar todos los campos');
+        return 
+    }
+
+    const body = new FormData(formulario)
+    const url = '/final_IS2_martinez/API/grados/modificar';
+    const config = {
+        method : 'POST',
+        body
+    }
+
+    try {
+        // fetch(url, config).then( (respuesta) => respuesta.json() ).then(d => data = d)
+        const respuesta = await fetch(url, config)
+        const data = await respuesta.json();
+        
+        const {codigo, mensaje,detalle} = data;
+        let icon = 'info'
+        switch (codigo) {
+            case 1:
+                formulario.reset();
+                icon = 'success'
+                buscar();
+                cancelarAccion();
+                break;
+        
+            case 0:
+                icon = 'error'
+                console.log(detalle)
+                break;
+        
+            default:
+                break;
+        }
+
+        Toast.fire({
+            icon,
+            text: mensaje
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const eliminar = async (id) => {
+    if(await confirmacion('warning','¿Desea eliminar este registro?')){
+        const body = new FormData()
+        body.append('gra_id', id)
+        const url = '/final_IS2_martinez/API/grados/eliminar';
+        const config = {
+            method : 'POST',
+            body
+        }
+        try {
+            const respuesta = await fetch(url, config)
+            const data = await respuesta.json();
+            console.log(data)
+            const {codigo, mensaje,detalle} = data;
+    
+            let icon = 'info'
+            switch (codigo) {
+                case 1:
+                    icon = 'success'
+                    buscar();
+                    break;
+            
+                case 0:
+                    icon = 'error'
+                    console.log(detalle)
+                    break;
+            
+                default:
+                    break;
+            }
+    
+            Toast.fire({
+                icon,
+                text: mensaje
+            })
+    
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+buscar();
+formulario.addEventListener('submit', guardar )
+btnBuscar.addEventListener('click', buscar)
+btnCancelar.addEventListener('click', cancelarAccion)
+btnModificar.addEventListener('click', modificar)
+
     
