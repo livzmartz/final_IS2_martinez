88114985b5362dc49041426c59a8f126638a1017
Livni <llmc0383@gmail.com>
2023-08-05 11:39:25 -0600

    se creo el modelo de la tabla de asignacion de los programadores a las apps

---
+++ models/Asignar.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Model;
+
+class Asignar extends ActiveRecord{
+
+    public static $tabla = 'asig_programadores';
+    public static $columnasDB = ['asig_programador','asig_app','asig_sit'];
+    public static $idTabla = 'asig_id';
+
+    public $asig_id;
+    public $asig_programador;
+    public $asig_app;
+    public $asig_sit;
+
+    public function __construct($args = []) {
+        $this->asig_id = $args['asig_id'] ?? null;
+        $this->asig_programador = $args['asig_programador'] ?? '';
+        $this->asig_app = $args['asig_app'] ?? '';
+        $this->asig_sit = $args['asig_sit'] ?? 1;
+    }
+
+}
