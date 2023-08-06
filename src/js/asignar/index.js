9115d963fc83ac03d09f5e600741faa122c91747
Livni <llmc0383@gmail.com>
2023-08-06 02:10:24 -0600

    se creo el index de js para la asignacion de app a los programadores

---
+++ src/js/asignar/index.js
@@ -0,0 +1,256 @@
+import { Dropdown } from "bootstrap";
+import Swal from "sweetalert2";
+import { validarFormulario, Toast, confirmacion} from "../funciones";
+
+const formulario = document.getElementById('formulario');
+const tablaAsignar = document.getElementById('tablaAsignar');
+const btnBuscar = document.getElementById('btnBuscar');
+const btnModificar = document.getElementById('btnModificar');
+const btnGuardar = document.getElementById('btnGuardar');
+const btnCancelar = document.getElementById('btnCancelar');
+const divTabla = document.getElementById('divTabla');
+
+btnModificar.disabled = true;
+btnModificar.parentElement.style.display = 'none';
+btnCancelar.disabled = true;
+btnCancelar.parentElement.style.display = 'none';
+
+const guardar = async (evento) => {
+    evento.preventDefault();
+    if (!validarFormulario(formulario, ['asig_id', 'asig_app', 'asig_programador'])) {
+        Toast.fire({
+            icon: 'info',
+            text: 'Debe llenar todos los datos'
+        });
+        return;
+    }
+
+    const body = new FormData(formulario);
+    body.delete('asig_id');
+    const url = '/final_IS2_martinez/API/asignar/guardar';
+    const config = {
+        method: 'POST',
+        body
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        const { codigo, mensaje, detalle } = data;
+        let icon = 'info';
+        switch (codigo) {
+            case 1:
+                formulario.reset();
+                icon = 'success';
+                buscar();
+                break;
+
+            case 0:
+                icon = 'error';
+                console.log(detalle);
+                break;
+
+            default:
+                break;
+        }
+
+        Toast.fire({
+            icon,
+            text: mensaje
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const buscar = async () => {
+    let asig_programador = formulario.asig_programador.value;
+    let asig_app = formulario.asig_app.value;
+    const url = `/final_IS2_martinez/API/asignar/buscar?asig_programador=${asig_programador}&asig_app=${asig_app}`;
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        tablaAsignar.tBodies[0].innerHTML = '';
+        const fragment = document.createDocumentFragment();
+
+        if (data.length > 0) {
+            let contador = 1;
+            data.forEach(asignacion => {
+                // CREAMOS ELEMENTOS
+                const tr = document.createElement('tr');
+                const td1 = document.createElement('td');
+                const td2 = document.createElement('td');
+                const td3 = document.createElement('td');
+                const td4 = document.createElement('td');
+                const buttonModificar = document.createElement('button');
+                const buttonEliminar = document.createElement('button');
+
+                // CARACTERISTICAS A LOS ELEMENTOS
+                buttonModificar.classList.add('btn', 'btn-warning');
+                buttonEliminar.classList.add('btn', 'btn-danger');
+                buttonModificar.textContent = 'Modificar';
+                buttonEliminar.textContent = 'Eliminar';
+
+                buttonModificar.addEventListener('click', () => colocarDatos(asignacion));
+                buttonEliminar.addEventListener('click', () => eliminar(asignacion.asig_id));
+
+                td1.innerText = contador;
+                td2.innerText = asignacion.asig_programador;
+                td3.innerText = asignacion.asig_app;
+
+                // ESTRUCTURANDO DOM
+                td4.appendChild(buttonModificar);
+                td5.appendChild(buttonEliminar);
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                tr.appendChild(td4);
+                tr.appendChild(td5);
+
+                fragment.appendChild(tr);
+
+                contador++;
+            });
+        } else {
+            const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            td.innerText = 'No existen registros';
+            td.colSpan = 5;
+            tr.appendChild(td);
+            fragment.appendChild(tr);
+        }
+
+        tablaAsignar.tBodies[0].appendChild(fragment);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const colocarDatos = (datos) => {
+    formulario.asig_programador.value = datos.asig_programador;
+    formulario.asig_app.value = datos.asig_app;
+    formulario.asig_id.value = datos.asig_id;
+
+    btnGuardar.disabled = true;
+    btnGuardar.parentElement.style.display = 'none';
+    btnBuscar.disabled = true;
+    btnBuscar.parentElement.style.display = 'none';
+    btnModificar.disabled = false;
+    btnModificar.parentElement.style.display = '';
+    btnCancelar.disabled = false;
+    btnCancelar.parentElement.style.display = '';
+    divTabla.style.display = 'none';
+};
+
+const cancelarAccion = () => {
+    btnGuardar.disabled = false;
+    btnGuardar.parentElement.style.display = '';
+    btnBuscar.disabled = false;
+    btnBuscar.parentElement.style.display = '';
+    btnModificar.disabled = true;
+    btnModificar.parentElement.style.display = 'none';
+    btnCancelar.disabled = true;
+    btnCancelar.parentElement.style.display = 'none';
+    divTabla.style.display = '';
+};
+
+const modificar = async () => {
+    if (!validarFormulario(formulario)) {
+        alert('Debe llenar todos los campos');
+        return;
+    }
+
+    const body = new FormData(formulario);
+    const url = '/final_IS2_martinez/API/asignar/modificar'; 
+    const config = {
+        method: 'POST',
+        body
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        const { codigo, mensaje, detalle } = data;
+        let icon = 'info';
+        switch (codigo) {
+            case 1:
+                formularioAsignar.reset();
+                icon = 'success';
+                buscar();
+                cancelarAccion();
+                break;
+
+            case 0:
+                icon = 'error';
+                console.log(detalle);
+                break;
+
+            default:
+                break;
+        }
+
+        Toast.fire({
+            icon,
+            text: mensaje
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const eliminar = async (id) => {
+    if (await confirmacion('warning', '¿Desea eliminar este registro?')) {
+        const body = new FormData();
+        body.append('asig_id', id);
+        const url = '/final_IS2_martinez/API/asignar/eliminar'; 
+        const config = {
+            method: 'POST',
+            body
+        };
+        try {
+            const respuesta = await fetch(url, config);
+            const data = await respuesta.json();
+
+            const { codigo, mensaje, detalle } = data;
+
+            let icon = 'info';
+            switch (codigo) {
+                case 1:
+                    icon = 'success';
+                    buscar();
+                    break;
+
+                case 0:
+                    icon = 'error';
+                    console.log(detalle);
+                    break;
+
+                default:
+                    break;
+            }
+
+            Toast.fire({
+                icon,
+                text: mensaje
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+buscar();
+formularioAsignar.addEventListener('submit', guardar);
+btnBuscar.addEventListener('click', buscar);
+btnCancelar.addEventListener('click', cancelarAccion);
+btnModificar.addEventListener('click', modificar);
