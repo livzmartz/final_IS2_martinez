8f425f9ae06c99bc9209007d08e24c2360817dbe
Livni <llmc0383@gmail.com>
2023-08-07 07:57:12 -0600

    correccion

--- models/Programadores.php
+++ models/Programadores.php
@@ -4,23 +4,23 @@ namespace Model;
 
 class Programadores extends ActiveRecord{
     public static $tabla = 'programadores';
-    public static $columnasDB = ['prog_correo','prog_grado','prog_nombres','prog_apellidos','prog_sit'];
+    public static $columnasDB = ['prog_grado','prog_nombres','prog_apellidos','prog_correo','prog_sit'];
     public static $idTabla = 'prog_id';
 
     public $prog_id;
-    public $prog_correo;
     public $prog_grado;
     public $prog_nombres;
     public $prog_apellidos;
+    public $prog_correo;
     public $prog_sit;
 
     public function __construct($args = []) 
     {
         $this->prog_id = $args['prog_id'] ?? null;
-        $this->prog_correo = $args['prog_correo'] ?? '';
         $this->prog_grado = $args['prog_grado'] ?? '';
         $this->prog_nombres = $args['prog_nombres'] ?? '';
         $this->prog_apellidos = $args['prog_apellidos'] ?? '';
+        $this->prog_correo = $args['prog_correo'] ?? '';
         $this->prog_sit = $args['prog_sit'] ?? 1;
     }
 
