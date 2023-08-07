7dd2245353e5b98aaced7f9bf221b5cda05ddbae
Livni <llmc0383@gmail.com>
2023-08-06 18:06:42 -0600

    modificacion de nombre de carpetas, en virtud que no funcionaba el anterior

---
+++ models/Aplicaciones.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Model;
+
+class Aplicaciones extends ActiveRecord{
+    public static $tabla = 'aplicaciones';
+    public static $columnasDB = ['app_nombre','app_estado'];
+    public static $idTabla = 'app_id';
+
+    public $app_id;
+    public $app_nombre;
+    public $app_estado;
+
+    public function __construct($args = [] )
+    {
+        $this->app_id = $args['app_id'] ?? null;
+        $this->app_nombre = $args['app_nombre'] ?? '';
+        $this->app_estado = $args['app_estado'] ?? 1;
+    }
+
+}
